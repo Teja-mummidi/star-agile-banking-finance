c5ff3b963eb119fc03120caccf96fd460cf6e178
Teja-mummidi <92073375+Teja-mummidi@users.noreply.github.com>
2024-10-19 04:07:25 +0530

    Updated main.tf

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "test-server" {
-  ami = "ami-06b21ccaeff8cd686"
+  ami = "ami-050cd642fd83388e4"
   instance_type = "t2.micro"
   key_name = "mykey"
   vpc_security_group_ids = ["sg-04cf4e3801bd09203"]
